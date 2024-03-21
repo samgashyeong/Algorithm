64a38e41a8db45af8d5937ccf171479220d5c41c
이준상 <66434787+samgashyeong@users.noreply.github.com>
2024-03-22 01:38:09 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 872 ms, Memory: 238912 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            bw.write(Integer.toString(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())));
+            bw.newLine();
+        }
+
+
+        bw.close();
+    }
+}
