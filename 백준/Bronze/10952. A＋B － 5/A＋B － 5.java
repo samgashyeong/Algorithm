ed8832639b78dd02b561de3fe1d273901f0961d2
이준상 <66434787+samgashyeong@users.noreply.github.com>
2024-03-21 16:28:12 +0900

    [Bronze V] Title: A+B - 5, Time: 136 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+ 
+public class Main {
+    static boolean[][] arr = new boolean[15][15];
+
+	public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        while(true){
+            st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            if(n == 0 && m == 0){
+                break;
+            }
+            else{
+                System.out.println(n+m);
+            }
+        }
+
+        
+
+	}
+}
