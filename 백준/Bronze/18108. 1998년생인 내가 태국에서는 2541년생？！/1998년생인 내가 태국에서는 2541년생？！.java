86a4301686615d6efbf8e2461bf33310d572dc9e
Lee Jun Sang <66434787+samgashyeong@users.noreply.github.com>
2024-08-02 09:22:00 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 80 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,22 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.math.MathContext;
+
+public class Main {
+    static int[] coin;
+    static int n,k;
+    static int[][] dp = new int[3][1010];
+    static long[] a = new long[90];
+    static int[][] arr = new int[3][1010];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int in = Integer.parseInt(br.readLine());
+
+        System.out.println(in-543);
+    }
+}
