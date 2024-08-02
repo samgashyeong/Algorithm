2954272e4ff4990a77a7a6bb928724695b72bf69
Lee Jun Sang <66434787+samgashyeong@users.noreply.github.com>
2024-08-02 09:29:26 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11372 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,26 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+import java.math.MathContext;
+
+public class Main {
+    static int[] coin;
+    static int n,k;
+    static int[][] dp = new int[3][1010];
+    static long[] a = new long[90];
+    static int[][] arr = new int[3][1010];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
