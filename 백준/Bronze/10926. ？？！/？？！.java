02b837d24a0793fa3cae560e474c2c5d28f44c93
Lee Jun Sang <66434787+samgashyeong@users.noreply.github.com>
2024-08-02 09:18:52 +0900

    [Bronze V] Title: ??!, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,22 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.math.MathContext;
+
+public class Main {
+    static int[] coin;
+    static int n,k;
+    static int[][] dp = new int[3][1010];
+    static long[] a = new long[90];
+    static int[][] arr = new int[3][1010];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String in = br.readLine();
+
+        System.out.println(in+"??!");
+    }
+}
