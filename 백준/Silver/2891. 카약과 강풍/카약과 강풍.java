487d2c1ae2da4e2cea85651abcbdb182d504da22
Lee Jun Sang <66434787+samgashyeong@users.noreply.github.com>
2024-08-14 22:12:27 +0900

    [Silver V] Title: 카약과 강풍, Time: 60 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/2891. 카약과 강풍/카약과 강풍.java
+++ 백준/Silver/2891. 카약과 강풍/카약과 강풍.java
@@ -13,44 +13,51 @@ public class Main {
         int s = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
 
-        int[] a = new int[n + 1];  // 팀 번호는 1부터 시작하므로 크기는 n+1로
+        int[] a = new int[15];
 
-        Arrays.fill(a, 1);  // 모든 팀에 1개의 카약을 배정
+        Arrays.fill(a, 1);
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < s; i++) {
-            int damagedTeam = Integer.parseInt(st.nextToken());
-            a[damagedTeam] = 0;  // 손상된 팀의 카약 수를 0으로 설정
+
+        for(int i=0;i<s;i++){
+            a[Integer.parseInt(st.nextToken())] = 0;
         }
 
         st = new StringTokenizer(br.readLine());
-        ArrayList<Integer> reserveTeams = new ArrayList<>();
-        for (int i = 0; i < r; i++) {
-            int reserveTeam = Integer.parseInt(st.nextToken());
-            if (a[reserveTeam] == 0) {
-                a[reserveTeam] = 1;  // 손상된 팀이 여분의 카약을 가져온 경우, 바로 그 팀이 사용
+
+        ArrayList<Integer> arr= new ArrayList<>();
+        for(int i=0;i<r;i++){
+            int num = Integer.parseInt(st.nextToken());
+            if (a[num] == 0) {
+                a[num] = 1;  // 손상된 팀이 여분의 카약을 가져온 경우, 바로 그 팀이 사용
             } else {
-                reserveTeams.add(reserveTeam);
+                arr.add(num);
             }
         }
 
-        // 남은 여분의 카약을 인접한 팀에게 빌려줄 수 있는지 확인
-        Collections.sort(reserveTeams);
-        for (int reserveTeam : reserveTeams) {
-            if (reserveTeam > 1 && a[reserveTeam - 1] == 0) {
-                a[reserveTeam - 1] = 1;  // 앞팀에게 빌려줌
-            } else if (reserveTeam < n && a[reserveTeam + 1] == 0) {
-                a[reserveTeam + 1] = 1;  // 뒷팀에게 빌려줌
+        Collections.sort(arr);
+
+        //System.out.println(arr);
+        for(int i=0;i<arr.size();i++){
+            int num = arr.get(i);
+            if(num-1>=1 && a[num-1] == 0){
+                a[num-1]++;
+            }
+            else if(num+1<=n && a[num+1] == 0){
+                a[num+1]++;
             }
         }
 
-        int cnt = 0;
-        for (int i = 1; i <= n; i++) {
-            if (a[i] == 0) {
-                cnt++;
+        //System.out.println(Arrays.toString(Arrays.stream(a).toArray()));
+
+        int sum = 0;
+        for(int i=1;i<=n;i++){
+            if(a[i] == 0){
+                sum++;
             }
         }
 
-        System.out.println(cnt);
+        System.out.println(sum);
+
     }
 }
