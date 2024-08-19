54d6f29b6ecdf44bd67e2edac6a388611c733367
Lee Jun Sang <66434787+samgashyeong@users.noreply.github.com>
2024-08-19 21:51:44 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 64 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,16 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+import java.math.MathContext;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        System.out.println(br.readLine().length());
+    }
+}
