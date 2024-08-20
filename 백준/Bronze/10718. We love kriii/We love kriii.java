4588cc3ab52c2e2f585bc4f6a4d235c2b95d2cf7
Lee Jun Sang <66434787+samgashyeong@users.noreply.github.com>
2024-08-20 23:28:18 +0900

    [Bronze V] Title: We love kriii, Time: 56 ms, Memory: 11392 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,17 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+import java.math.MathContext;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        System.out.println("강한친구 대한육군\n" +
+                "강한친구 대한육군");
+    }
+}
