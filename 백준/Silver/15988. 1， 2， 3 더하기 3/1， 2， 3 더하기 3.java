cece0832c095eb3ff8ec36cb96d22b84edd376da
Lee Jun Sang <samgashyeong@naver.com>
2024-09-25 11:52:53 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 104 ms, Memory: 19944 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[1000003];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 1000000; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
+        }
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
